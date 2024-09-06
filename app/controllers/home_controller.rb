ff6eee83564baeaccf3ce81aff4cb49ac14c0fac
Francisco Opazo <francisco.opazo@uc.cl>
2024-09-06 19:51:55 -0400

    Render con rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
+# Para corregir error de rubocop, se debe agregar la siguiente línea:
+# actualización de render
 class HomeController < ApplicationController
-    def index
-      # Aquí puedes añadir lógica si es necesario, o simplemente renderizar la vista por defecto.
-    end
+  def index
+    # Aquí puedes añadir lógica si es necesario, o simplemente renderizar la vista por defecto.
   end
-  
+end
