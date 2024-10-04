4b7b134cada0950206bcda0c00d424577fdacd98
José Ignacio Duarte <joseignacioduarte@uc.cl>
2024-10-04 16:32:59 -0300

    alerta lista

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   
     # Redirigir al inicio de sesión después de registrarse, pero antes de la confirmación
     def after_inactive_sign_up_path_for(resource)
+      flash[:notice] = "Por favor confirma tu cuenta a través del enlace que hemos enviado a tu correo electrónico."
       new_session_path(resource_name) # Redirige a la página de inicio de sesión
     end
   end
