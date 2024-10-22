c42cb95c77d8aa3f95b97ba6c9359d606c4e3f10
José Ignacio Duarte <joseignacioduarte@uc.cl>
2024-10-22 13:54:36 -0300

    checkpoint grupos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,34 @@
-//= require rails-ujs
+document.addEventListener('DOMContentLoaded', function() {
+    const filtroAlumnosInput = document.getElementById('filtro-alumnos');
+    const alumnosList = document.querySelectorAll('.alumno-item');
+  
+    // Filtro de alumnos
+    filtroAlumnosInput.addEventListener('input', function() {
+      const filtro = filtroAlumnosInput.value.toLowerCase();
+  
+      alumnosList.forEach(function(alumnoItem) {
+        const numeroAlumno = alumnoItem.getAttribute('data-numero');
+        
+        if (numeroAlumno.includes(filtro)) {
+          alumnoItem.style.display = 'block';
+        } else {
+          alumnoItem.style.display = 'none';
+        }
+      });
+    });
+  
+    // Manejo de la alerta
+    const alert = document.querySelector('.alert-notice');  // Selecciona el elemento con la clase .alert
+  
+    if (alert) {
+        // Hace que la alerta desaparezca después de 2 segundos
+        setTimeout(function() {
+        alert.style.opacity = '0'; // Cambiar la opacidad para desaparecer lentamente
+        }, 2000); // Desaparece después de 2 segundos
+
+        // Elimina la alerta del DOM después de que la transición termine
+        setTimeout(function() {
+        alert.remove();
+        }, 2500); // Elimina el nodo después de 0.5 segundos
+    }
+});
--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -2,4 +2,5 @@ class Alumno < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :grupos
+  validates :numero_alumno, presence: true
 end
